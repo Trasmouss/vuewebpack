535197c131a972723e4011721cb1f40e972bd395
Cihan Demir <cihandemir7@hotmail.com>
2023-02-03 14:33:24 +0300

    add start files

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="text">
+    <h3>{{text}}</h3>
+  </div>
+</template>
+
+<script>
+    export default {
+        name:"app",
+        data(){
+            return {
+                text: 'Wellcome to Vue'
+            }
+        }
+    };
+</script>
+
+<style>
+    #text{
+        color: red;
+    }
+</style>
