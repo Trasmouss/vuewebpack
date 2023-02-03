dacf0741e4781b03f46ed346170736d490935775
Cihan Demir <cihandemir7@hotmail.com>
2023-02-03 17:30:36 +0300

    solve Cannot GET problem

--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,17 @@ const path = require('path');
 const VueLoaderPlugin = require('vue-loader/lib/plugin');
 
 module.exports = {
+    mode: 'development',
     entry : path.join(__dirname, 'src', 'main.js'),
     output : {
         path : path.join(__dirname, 'dist'),
         publicPath : '/dist/',
         filename : 'build.js'
+    },
+    devServer: {
+        static: {
+            directory: path.join(__dirname, '/')
+        }
     },
     module:{
         rules:[
